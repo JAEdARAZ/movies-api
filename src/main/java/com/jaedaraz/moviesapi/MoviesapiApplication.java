bb1b75b1e2f60c11e58a408bc946060842cb9ded
Jaime Aranzazu <jaedaraz@gmail.com>
2021-02-10 20:11:03 +0100

    añadida funcionalidad de caché básica

--- src/main/java/com/jaedaraz/moviesapi/MoviesapiApplication.java
+++ src/main/java/com/jaedaraz/moviesapi/MoviesapiApplication.java
@@ -2,12 +2,14 @@ package com.jaedaraz.moviesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableCaching
 public class MoviesapiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/jaedaraz/moviesapi/rest/MovieRestController.java
+++ src/main/java/com/jaedaraz/moviesapi/rest/MovieRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,6 +30,7 @@ public class MovieRestController {
 	private ObjectMapper objectMapper;
 	
 	
+	@Cacheable("movieTitle")
 	@GetMapping("/movies")
 	public List<Movie> getMovieByName(@RequestParam("movieTitle") String movieTitle) {
 		String responseJson = webClientBuilder.build()
